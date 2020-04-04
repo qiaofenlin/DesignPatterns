973108c72f35c3e11037125affcb43f1c478ebce
qiaofenlin <790872612@qq.com>
2020-04-04 21:23:18 +0800

    设计模式继续

--- src/main/java/com/mashibing/dp/singleton/Mgr06.java
+++ src/main/java/com/mashibing/dp/singleton/Mgr06.java
@@ -23,6 +23,34 @@ public class Mgr06 {
                         e.printStackTrace();
                     }
                     INSTANCE = new Mgr06();
+                    /*
+                    * JVM编译器编译完成之后，创建对象的指令分为3步:
+                    * 1.分配(申请)内存，赋初始值int 0
+                    * 2.成员变量初始化 具体值 int a=8
+                    * 3.将变量的指针赋值给`INSTANCE`
+                    *
+                    * 在极高并发的状态，编译器会进行指令重排序，一个线程如果创建`INSTANCE`的指令执行到第一步时，就执行3，
+                    * 第二个线程进来进行
+                    *    if(INSTANCE == null)  INSTANCE 不是null了，已经初始化了对应的文档，
+                    * 所以第二个线程进来之后直接就返回了，但是单例对象完全创建完成。容易造成问题。
+                    *
+                    * 如果单例对象加了volatile修饰，根据volatile的禁止指令重排序，会依次执行123,最后给`INSTANCE`赋值。
+                    *
+                    * public class T {
+                        public static void main(String[] args) {
+                            T t = new T();
+                        }
+                      }
+                    *
+                    *
+                    *
+                    *   0 new #2 <com/mashibing/jvm/c0_basic/T>     分配内存
+                        3 dup
+                        4 invokespecial #3 <com/mashibing/jvm/c0_basic/T.<init>> 调用构造方法 将成员变量初始化
+                        7 astore_1      赋值给引用
+                        8 return
+                    *
+                     * */
                 }
             }
         }
